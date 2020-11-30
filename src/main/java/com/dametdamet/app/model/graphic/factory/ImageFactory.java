813477fccc378eb8c65259bd333f708a095e8e8e
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2020-11-30 13:45:10 +0100

    Affichage ghost

--- src/main/java/com/dametdamet/app/model/graphic/factory/ImageFactory.java
+++ src/main/java/com/dametdamet/app/model/graphic/factory/ImageFactory.java
@@ -197,6 +197,8 @@ public class ImageFactory {
                 return hero[numSprite];
             case MONSTER:
                 return monster[numSprite];
+            case GHOST:
+                return ghost[numSprite];
             default:
                 return entityNotFound;
         }
